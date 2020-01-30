f7cb3b3d2acc7e5e876a1a1be918e7540e636e8d
Prabakaran T <mailtodecipher@gmail.com>
2020-01-30 18:37:21 +0530

    added helper & bs-4 views updated

---
+++ src/Helper/Ltrans.php
@@ -0,0 +1,23 @@
+<?php
+namespace Webtamizhan\Ltrans;
+
+
+class Ltrans{
+
+    function langOption()
+    {
+        $path = base_path() . '/resources/lang/';
+        $lang = scandir($path);
+        $files = array_diff($lang, array('.', '..','vendor'));
+        $t = array();
+        foreach ($files as $value) {
+            if (is_dir($path . $value)) {
+                $fp = file_get_contents($path . $value . '/config.json');
+                $fp = json_decode($fp, true);
+                $t[] = $fp;
+            }
+        }
+        return $t;
+    }
+
+}
